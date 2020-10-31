79aba8f8386fe8b5fb087c88c404c3323f142013
Aranteus <https://github.com/Aranteus>
2020-10-31 13:19:04 +0500

    Prediction model was added

--- backend/chatbot text analyser.py
+++ backend/chatbot text analyser.py
@@ -1,5 +1,5 @@
-import nltk
-nltk.download('stopwords')
+# import nltk
+# nltk.download('stopwords')
 
 from nltk.corpus import stopwords
 from pymystem3 import Mystem
@@ -7,12 +7,26 @@ from string import punctuation
 rus_stopwords = stopwords.words("russian")
 rus_stopwords.remove("не")
 
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
 
-def analyser(request: str):
+
+def analyser(request: str, model: Pipeline):
+    formatted_request = _lemmatize(request)
+    prediction = model.predict(formatted_request)
+    return prediction
+
+
+def _lemmatize(request: str):
     no = False
-    mystem = Mystem()
+    m = Mystem()
     tokens = []
-    lemmas = mystem.lemmatize(text=request.lower())
+
+    request.replace("\n", "")
+    lemmas = m.lemmatize(text=request.lower())
 
     for lemma in lemmas:
         if lemma not in rus_stopwords and lemma != " " and lemma.strip() not in punctuation:
@@ -26,14 +40,43 @@ def analyser(request: str):
                 tokens.append(lemma)
 
     formatted_request = " ".join(tokens)
-    print(request.strip())
-    print(tokens)
-    print(formatted_request, '\n')
+    # print(request.strip())
+    # print(tokens)
+    # print(formatted_request, '\n')
+    return formatted_request
+
+
+def _train(train_set: str, groups: list[str], testing: bool = False):
+
+    X = train_set
+    y = groups
+
+    if testing:
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4)  # not obligatory
+    else:
+        X_train, y_train = X, y
+
+    model = Pipeline([
+        (CountVectorizer(ngram_range=(1, 2))),
+        (TfidfTransformer()),
+        (MultinomialNB())
+    ])
+
+    model = model.fit(X_train, y_train)
+
+    if testing:
+        predictions = model.predict(X_test)
+        print('Model score:', model.score(X_test, y_test))
+
+    return model
 
 
 if __name__ == "__main__":
+    # train_set = ""
+    # model = _train(train_set)
+
     with open("samples.txt") as file:
         samples = file.readlines()
         for sample in samples:
-            sample.replace("\n", "")
             analyser(sample)
+
